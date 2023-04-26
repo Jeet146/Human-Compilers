f6823796de615a1ac827ede6d1947f745fd0c3f6
Triparna kar <kartriparna53@gmail.com>
2023-04-26 10:48:06 +0530

    Added comments

--- Mini_Project.cpp
+++ Mini_Project.cpp
@@ -1,85 +1,110 @@
+/*This program is prepared by Ayush Mistri (22CS041)
+                              Triparna Kar (22CS029)
+                              Dev Patel    (22CS052)    */
+
 #include <iostream>
 #include <iomanip>
 #include <math.h>
+#include<cstring>
+
 using namespace std;
 
-class Home_loan{
-    public:
+class Home_loan   //creating a class to calculate home loan interest
+{
+    private:     //Declaring data members as private
     string Author_Name;
     float EMI, INT, Rate, Rate_per_month, Loan_amount, Balance=0;
-    float Total_INT=0, Princ=0, xyz;
+    float Total_INT=0, Principle_amt=0, xyz;
     int year,months;
-    void getdata(){
+
+    public:     //Declaring and Defining member fuction in public access mode
+    void getdata()      //creating getdata() fuction to input data
+    {
         cout<<"Enter a Author name : ";
-        cin>>Author_Name;
+        getline(cin,Author_Name);
         cout<<"Enter a amount of Loan : ";
         cin>>Loan_amount;
-        xyz=Loan_amount;
-        cout<<"Enter a Rate for Loan : ";
+        xyz=Loan_amount;        //Assigning Loan_amount in another member xyz for later use
+        cout<<"Enter a Rate of interest : ";
         cin>>Rate;
-        cout<<"Enter a Tenure in year : ";
+        cout<<"Enter a Tenure in years : ";
         cin>>year;
     }
-    void putdata(){
+
+    void title()        //Function to print the title of calculator
+    {
         cout<<endl;
         cout<<"\t\t***********************************************************************************"<<endl;
         cout<<"\t\t\t\t\t   HOME LOAN INTEREST CALCULATOR\t\t"<<endl;
         cout<<"\t\t***********************************************************************************\n"<<endl;
     }
-    void putdata_1(){
-        cout<<"\n";
-        cout<<"Author Name is : "<<Author_Name<<endl;
-        cout<<"Total Amount of Loan is : "<<Loan_amount<<endl;
-        cout<<"Rate for Loan is : "<<Rate<<endl;
-        cout<<"Tenure in month is : "<<year<<endl;
+
+    void putdata()      //Creating function to print the inputted data
+    {
+        cout<<"\n";     //Inserting a new line in output
+        cout<<"Author Name : "<<Author_Name<<endl;
+        cout<<"Total Amount of Loan : "<<Loan_amount<<endl;
+        cout<<"Rate of interest : "<<Rate<<endl;
+        cout<<"Tenure in month : "<<year<<endl;
     }
-    // xyz=Loan_amount;
-    void putdata_2(){
+    
+    void Loan_calculation()   //Fuction for calculation and print monthly amounts and interests
+    {
         cout<<"\n";
-        cout<<"Months\t\tLoan Amount\t\tEMI\t\tINT\t\tPrinc\t\tBalance\n\n";
-        for(int i=1 ; i<=(year*12) ; i++){
-            cout<<setw(16)<<left<<i;
-            for(int j=0 ; j<1 ; j++){
-                if(Balance!=0){
+        cout<<"Months\t\tLoan Amount\t\tEMI\t\tINT\t\tPrinciple_amt\t\tBalance\n\n";
+        for(int i=1 ; i<=(year*12) ; i++)       //outer loop for iterating months
+        {
+            cout<<setw(16)<<left<<i;       //Using setw and left manipulator for Output
+            for(int j=0 ; j<1 ; j++)    //inner loop for calculating monthly amounts 
+            {
+                if(Balance!=0)
+                {
                     Loan_amount=Balance;
                 }
                 cout<<fixed<<setprecision(3)<<setw(24)<<left<<Loan_amount;
-                if(i==1){
-                    EMI = cal_EMI();
+                if(i==1)
+                {
+                    EMI = cal_EMI();    //Assinging the returned value of fuction in EMI only once
+                                        //EMI is constant in program
                 }
                 cout<<setw(16)<<left<<EMI;
-                INT=Loan_amount*(Rate/(12*100));
+                
+                INT=Loan_amount*(Rate/(12*100));      //monthly interest calculation
                 cout<<setw(16)<<left<<INT;
-                Princ=EMI-INT;
-                cout<<setw(16)<<left<<Princ;
-                Balance=Loan_amount-Princ;
+
+                Principle_amt=EMI-INT;      //calculation for monthly principle Amount
+                cout<<setw(16)<<left<<Principle_amt;
+
+                Balance=Loan_amount-Principle_amt;      //Remaining balance amount
                 if(Balance<0){
-                    cout<<"0.000"<<endl;
+                    cout<<"0.000"<<endl;  //Final balance amount
                 }
                 else{
-                    cout<<setw(20)<<left<<Balance<<endl;
+                    cout<<setw(20)<<left<<Balance<<endl;    //Monthly balance remaining
                 }
-                Total_INT = Total_INT + INT;
+                Total_INT = Total_INT + INT;    //Total interest amount on loan
            }
         }
-        cout<<"\nTotal amount of Interest is : "<<Total_INT<<endl;
-        cout<<"Total amount paied by User is : "<<xyz+Total_INT<<endl;
+        cout<<"\nTotal amount of Interest : "<<Total_INT<<endl;
+        cout<<"Total amount paied by User : "<<xyz+Total_INT<<endl; 
     }
-    float cal_EMI(){
+    float cal_EMI()     //Function to calculate the EMI
+    {
         Rate_per_month=Rate/(12*100);
         months=year*12;
         EMI=(Loan_amount*Rate_per_month*pow(Rate_per_month+1,months)/(pow(1+Rate_per_month,months)-1));
+                        //Formula to calculate EMI
         return EMI;
     }
 
 };
 
 int main(){
-    Home_loan h1;
-    h1.putdata();
+    Home_loan h1;   //Creating object of class Home_loan
+    h1.title();     //calling the respective member fuctions through object
     h1.getdata();
-    h1.putdata_1();
-    h1.putdata_2();
+    h1.putdata();
+    h1.Loan_calculation();
     
     return 0;
 }
